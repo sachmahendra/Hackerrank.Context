658e0e53cb3fe3463a7484eee51c524d683b5b44
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-06-27 11:01:40 +0530

    initial.commit
    IT IS A JAVA CODE FOR CIRCLE AND CYLINDER CLASS USING INHERITANCE

---
+++ CircleAndCylinder.java
@@ -0,0 +1,29 @@
+class Circle{
+    public double radius;
+    public double area(){
+        return Math.PI * radius *radius;
+    }
+    public double perimeter(){
+        return 2*Math.PI * radius;
+    }
+}
+
+class Cylinder extends Circle{
+
+    public double height;
+    public double volume(){
+        return area()* height;
+    }
+
+}
+
+public class CircleAndCylinder {
+public static void main(String[] args) {
+    Cylinder c = new Cylinder();
+    c.radius = 5;
+    c.height = 10;
+    System.out.println(c.area());
+    System.out.println(c.volume());
+
+}
+}
